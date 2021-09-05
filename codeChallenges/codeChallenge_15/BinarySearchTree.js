964cee5827054992c3573c5412f07e9221984b95
AlaaN-Smadi <alaasmadi1010@gmail.com>
2021-09-06 00:29:42 +0300

    Solve challenge 16

--- codeChallenges/codeChallenge_15/BinarySearchTree.js
+++ codeChallenges/codeChallenge_15/BinarySearchTree.js
@@ -10,21 +10,27 @@ class BinarySearchTree extends BinaryTree {
 
         if (this.root == null) {
             this.root = node;
-        }else{
+        } else {
             let current = this.root
-            if(current.value>value){
-                if(current.left !== null){
-                    current = current.left
-                }else{
-                    current.left = node
-                }
-            }else{
-                if(current.right !== null){
-                    current = current.right
-                }else{
-                    current.right = node
+            while (current !== null) {
+                if (current.value > value) {
+                    if (current.left !== null) {
+                        current = current.left
+                    } else {
+                        current.left = node
+                        return('added')
+                    }
+                } else {
+                    if (current.right !== null) {
+                        current = current.right
+                    } else {
+                        current.right = node
+                        return('added')
+
+                    }
                 }
             }
+
         }
     }
 
